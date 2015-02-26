7726be03acbff7c1a707cf68cfc770189e90aa67
Kamil Davulcuoglu <kam-dav@hotmail.com>
2015-02-26 14:34:06 +0000

    removed muscle images, completed about page, to be changed to wp format

--- about.php
+++ about.php
@@ -20,8 +20,38 @@ get_header(); ?>
         </div>
     </div>
 
-    <div class="medium-5 columns animated bounceInRight">
-        <img class="page-img" src="<?php echo get_template_directory_uri(); ?>/assets/img/images/ram-fitness-raymond-1.jpg" />
+    <div class="medium-5 columns animated bounceInRight text-center">
+        <img class="page-img about-img" src="<?php echo get_template_directory_uri(); ?>/assets/img/images/ram-fitness-raymond-1.jpg" />
+    </div>
+</div>
+
+<div class="row space-top">
+    <div class="medium-6 columns animated bounceInRight text-center space-top">
+        <img class="page-img" src="<?php echo get_template_directory_uri(); ?>/assets/img/images/ram-fitness-raymond-2.png" />
+    </div>
+
+    <div class="medium-6 columns space-top animated bounceInRight text-center">
+        <h4 class="m-title">Below is a list of my Qualifications & Achievements:</h4>
+
+        <ul class="qualifications text-left space-top">
+            <li><i class="fi-check"></i> Level 3 Personal Trainer</li>
+            <li><i class="fi-check"></i> Level 2 Fitness Instructor</li>
+            <li><i class="fi-check"></i> Boxercise Personal Trainer</li>
+            <li><i class="fi-check"></i> Kickboxercise Instructor</li>
+            <li><i class="fi-check"></i> Boxercise Kids Instructor</li>
+            <li><i class="fi-check"></i> Kettlercise</li>
+            <li><i class="fi-check"></i> Level 1 & 2 Football Coaching</li>
+            <li><i class="fi-check"></i> Level 1 & 2 Key Skills</li>
+            <li><i class="fi-check"></i> Level 3 Employment Awareness</li>
+            <li><i class="fi-check"></i> Safe Guarding Children (FA)</li>
+            <li><i class="fi-check"></i> First Aid Work (Valid 3 Years)</li>
+            <li><i class="fi-check"></i> Emergency Heart Start Traing (Army)</li>
+            <li><i class="fi-check"></i> AED (automated external defibrillator)</li>
+            <li><i class="fi-check"></i> Muhammad Ali Boxing Fitness Trainer</li>
+            <li><i class="fi-check"></i> Boxercise Aerobics Instructor</li>
+            <li><i class="fi-check"></i> Advanced Boxercise Instructor</li>
+            <li><i class="fi-check"></i> Enhanched CRB Checked</li>
+        </ul>
     </div>
 </div>
 
